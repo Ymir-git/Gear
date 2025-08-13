5cb9d3cc00987f6b391f342b1c04a9cfe75e029c
Ymir-git <mirgorod.yuri@gmail.com>
2025-08-13 18:07:22 +0300

    add

--- scripts/header.js
+++ scripts/header.js
@@ -1,6 +1,17 @@
 const menuBtn = document.querySelector('.menu__btn');
 const menuList = document.querySelector('.header__inner-menu');
+const menuShadow = document.querySelector('.menu--close');
+const body = document.body;
 
 menuBtn.addEventListener('click',()=>{
   menuList.classList.toggle('header__inner-menu--open');
+  menuShadow.classList.toggle('menu--open');
+  body.classList.toggle('no-scroll');
+});
+menuShadow.addEventListener('click', function(event) {
+    if (event.target === menuShadow) {
+      menuList.classList.remove('header__inner-menu--open');
+      menuShadow.classList.remove('menu--open');
+      body.classList.remove('no-scroll');
+    }
 });
